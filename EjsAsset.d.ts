b36493e07d60397dac00f0f833d5a1fb5079693e
Jamie Gerrard Lievesley <jamie@jamiegl.co.uk>
2021-03-13 01:23:33 +0000

    Prettier ignore generated d.ts files

--- EjsAsset.d.ts
+++ EjsAsset.d.ts
@@ -4,10 +4,10 @@ export = EjsAsset;
  * @extends {Asset<ejs.ClientFunction>}
  */
 declare class EjsAsset extends Asset<ejs.ClientFunction> {
-  /**
-   * @param {string} name
-   * @param {{}} options
-   */
-  constructor(name: string, options: {});
+    /**
+     * @param {string} name
+     * @param {{}} options
+     */
+    constructor(name: string, options: {});
 }
 import { Asset } from "parcel-bundler";
